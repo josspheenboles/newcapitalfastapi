3ad825824a11072a8975d91de9cc0b6bfe1da10e
josspheenboles <eng.jospheenboles@gmail.com>
2026-08-01 00:02:48 +0300

    search by name in items & query parma

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
 items=[
-        {"id":1,"name":'laptop'},
-         {"id":2,"name":'keyboard'}
+        {"id":1,"name":'item1'},
+         {"id":2,"name":'item2'}
     ]
 
 # create app
@@ -18,6 +18,14 @@ def helloworld():
 def getitems():
     return items
 
+@app.get('/Items/search/')
+def getitembyname(name):
+    print(name)
+    for item in items:
+        if item['name']==name:
+            return item
+    return {'msg':'item not found'}
+
 @app.get('/Items/{item_id}/')
 def getitembyid(item_id:int):
     for item in items:
@@ -25,3 +33,6 @@ def getitembyid(item_id:int):
         if item['id']==(item_id):
             return item
     return {'msg':'item not found'}
+
+
+   
