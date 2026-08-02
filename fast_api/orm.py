07e31fd4d769f5d841312e00244975cc5441ac60
josspheenboles <eng.jospheenboles@gmail.com>
2026-08-02 21:42:08 +0300

    orm app-db session

--- fast_api/orm.py
+++ fast_api/orm.py
@@ -8,3 +8,13 @@ from models import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
