86355aa4d2eb31adadd1ecead3aff2c4b40d40a5
josspheenboles <eng.jospheenboles@gmail.com>
2026-08-02 21:33:48 +0300

    models-employee class

--- fast_api/orm.py
+++ fast_api/orm.py
@@ -19,3 +19,18 @@ class Department(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
     employees = relationship("Employee", back_populates="department")
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    fname = Column(String, index=True)
+    lname = Column(String, index=True)
+    salary = Column(Integer)
+    dno = Column(Integer, ForeignKey("departments.id"))
+    manager_id = Column(Integer, ForeignKey("employees.id"), nullable=True)
+
+    department = relationship("Department", back_populates="employees")
+    manager = relationship("Employee", remote_side=[id], back_populates="subordinates")
+    subordinates = relationship("Employee", back_populates="manager")
