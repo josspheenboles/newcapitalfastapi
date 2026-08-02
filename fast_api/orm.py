5d622c3e9b70901ea236a092f4a7808e518510d4
josspheenboles <eng.jospheenboles@gmail.com>
2026-08-02 21:32:06 +0300

    models-db engine & connect

--- fast_api/orm.py
+++ fast_api/orm.py
@@ -6,3 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
