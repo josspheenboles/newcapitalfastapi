c7dc340defbc83ef96dd9689dd097edb60c5bbc1
josspheenboles <eng.jospheenboles@gmail.com>
2026-08-02 21:39:48 +0300

    orm app-imports

--- fast_api/orm.py
+++ fast_api/orm.py
@@ -1,36 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-import os
-from dotenv import load_dotenv
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
 
-load_dotenv()  # Load environment variables from .env file
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
-# department table
-class Department(Base):
-    __tablename__ = "departments"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    employees = relationship("Employee", back_populates="department")
-
-
-class Employee(Base):
-    __tablename__ = "employees"
-
-    id = Column(Integer, primary_key=True, index=True)
-    fname = Column(String, index=True)
-    lname = Column(String, index=True)
-    salary = Column(Integer)
-    dno = Column(Integer, ForeignKey("departments.id"))
-    manager_id = Column(Integer, ForeignKey("employees.id"), nullable=True)
-
-    department = relationship("Department", back_populates="employees")
-    manager = relationship("Employee", remote_side=[id], back_populates="subordinates")
-    subordinates = relationship("Employee", back_populates="manager")
+import models
+import schemas
+from models import SessionLocal, engine
