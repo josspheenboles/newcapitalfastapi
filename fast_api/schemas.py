757176271feeffffe3356d67f905eacda552ba59
josspheenboles <eng.jospheenboles@gmail.com>
2026-08-02 21:36:49 +0300

    schemas-department contain list of employees

--- fast_api/schemas.py
+++ fast_api/schemas.py
@@ -3,3 +3,10 @@ from typing import Optional, List
 
 class DepartmentBase(BaseModel):
     name: str
+
+class Department(DepartmentBase):
+    id: int
+    employees: List["Employee"] = []
+
+    class Config:
+        from_attributes = True
