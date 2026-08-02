731873467a87e6067464fa37e5bd4f9d1e1a6569
josspheenboles <eng.jospheenboles@gmail.com>
2026-08-02 21:40:40 +0300

    orm app-app create

--- fast_api/orm.py
+++ fast_api/orm.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-
 import models
 import schemas
 from models import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
